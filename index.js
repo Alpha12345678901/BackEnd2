a8f65cca3abdc88d9b8c9d0a04b23f7d2e72f035
Jonathan Kloepfer <kloepj@rpi.edu>
2022-07-26 21:03:24 -0400

    Socket Work

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const httpServer = require("http").createServer(app);
 const options = {cors: {orgin:"*"}};
 const io = require("socket.io")(httpServer, options);
 io.on("connection", socket => {
-  socket.on("creat", socket=>{
-    socket.join("Room");
+  socket.on("creat", (params , callback)=>{
+    socket.join(socket.id);
+    console.log("HI");
   });
-  socket.on("join", socket=>{
-    //socket.join();
-    console.log(socket)
+  socket.on("join", (params, callback)=>{
+    socket.join(params);
+    console.log(params);
   });
 });
 
